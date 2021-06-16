1bc16de2c7d8a794d8d3c487e44ae1241f432868
Sri Harsha Paladugula <paladugula.sriharsha001@gmail.com>
2021-06-16 22:54:08 +0530

    added some code for circular linked list

--- Linked Lists/circular_linked_list.cpp
+++ Linked Lists/circular_linked_list.cpp
@@ -13,9 +13,14 @@ class CircularLinkedList
 private:
     Node* head;
 public:
+    CircularLinkedList() { head = NULL;};
     CircularLinkedList(int A[], int n);
     void Display();
     void recursiveDisplay(Node* p);
+    void create(int A[],int n);
+    int Length(struct Node *p);
+    void Insert(struct Node *p,int index, int x);
+    int Delete(struct Node *p,int index);
     Node* getHead(){ return head; }
     ~CircularLinkedList();
 
@@ -64,7 +69,106 @@ void CircularLinkedList::recursiveDisplay(Node *p)
     }
     flag = 0;
 }
- 
+
+void CircularLinkedList::create(int A[],int n)
+{
+    int i;
+    struct Node *t,*last;
+    head = new Node;
+    head->data=A[0];
+    head->next=head;
+    last=head;
+    
+    for(i=1;i<n;i++)
+    {
+        t=(struct Node*)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=last->next;
+        last->next=t;
+        last=t;
+    }
+}
+int CircularLinkedList::Length(struct Node *p)
+{
+    int len=0;
+    do
+    {
+        len++;
+        p=p->next;
+    }while(p!=head);
+    return len;
+}
+
+void CircularLinkedList::Insert(struct Node *p,int index, int x)
+{
+    struct Node *t;
+    int i;
+    if(index<0 || index > Length(p))
+        return;
+
+    if(index==0)
+    {
+        t=new Node;
+        t->data=x;
+        if(head==NULL)
+        {
+            head=t;
+            head->next=head;
+        }
+        else
+        {
+            while(p->next!=head)
+                p=p->next;
+            p->next=t;
+            t->next=head;
+            head=t;
+        }
+    }
+    else
+    {
+        for(i=0;i<index-1;i++)p=p->next;
+        t= new Node;
+        t->data=x;
+        t->next=p->next;
+        p->next=t;
+    }
+}
+
+int CircularLinkedList::Delete(struct Node *p,int index)
+{
+    struct Node *q;
+    int i,x;
+
+    if(index <0 || index >Length(head))
+        return -1;
+    if(index==1)
+    {
+        while(p->next!=head)
+            p=p->next;
+        x=head->data;
+        if(head==p)
+        {
+            free(head);
+            head=NULL;
+        }
+        else
+        {
+            p->next=head->next;
+            free(head);
+            head=p->next;
+        }
+    }
+    else
+    {
+        for(i=0;i<index-2;i++)
+            p=p->next;
+        q=p->next;
+        p->next=q->next;
+        x=q->data;
+        free(q);
+    }
+ return x;
+}
 CircularLinkedList::~CircularLinkedList() 
 {
     Node* p = head;
@@ -100,6 +204,19 @@ int main()
  
     Node* h = cl.getHead();
     cl.recursiveDisplay(h);
+    cout << "\n";
+
+    CircularLinkedList cllist;
+    int B[]={2,3,4,5,6};
+    cllist.create(B,5);
+
+    cllist.Display();
+
+    cllist.Insert(cllist.getHead(),5,8);
+    cllist.Display();
+
+    cllist.Delete(cllist.getHead(),6);
+    cllist.Display();
  
     return 0;
 }
